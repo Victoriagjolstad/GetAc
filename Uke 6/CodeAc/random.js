b9f3b013f56a1b39d916bd0e8f1acf2a4847fdef
Victoriagjolstad <116063769+Victoriagjolstad@users.noreply.github.com>
2022-12-02 15:24:10 +0100

    Create random.js

---
+++ Uke 6/CodeAc/random.js
@@ -0,0 +1,26 @@
+//magic eight ball wich generates a random numgber to answer your questions
+
+const userQuestion = 'what would you like to ask the Macig Eight Ball?';
+userName ? console.log(`${userName}` + ',' + userQuestion):
+console.log(userQuestion)
+
+var randomNumber = Math.floor(Math.random() * 7);
+console.log(randomNumber)
+
+let eightBall = randomNumber; 
+
+if (randomNumber == 0) {
+   console.log('Signs point to yes');}
+else if(randomNumber == 1) {
+  console.log('It is certain')
+} else if (randomNumber == 2) {
+  console.log('It is decidedly so')
+} else if (randomNumber == 3) {
+  console.log('Reply hazy try again')
+} else if (randomNumber == 4) {
+  console.log('Do not count on it')
+} else if (randomNumber == 5) {
+  console.log('My sources say no')
+} else if (randomNumber == 6) {
+    console.log('My sources say no')
+ } else {console.log('Outlook not so good') }
