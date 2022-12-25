8737092eef9c25855dae3c3a9c67fb7fd304e059
Victoriagjolstad <116063769+Victoriagjolstad@users.noreply.github.com>
2022-12-25 16:57:35 +0100

    Create forøktoloop.js

---
+++ Uke 5/Oppgave/forøktoloop.js
@@ -0,0 +1,14 @@
+show()
+function show() {
+    let HTML = '';
+    document.getElementById('app').innerHTML = HTML;
+}
+
+
+function reverseString () {
+   let text= "hello!"; 
+    for (let i = texten.length - 1; i > 0; i--){
+        text += texten[i];}
+        console.log(reverseString);
+        show ()
+    }
