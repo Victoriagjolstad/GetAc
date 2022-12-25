00e7dd785fffe3cb5fc6cf97f7a1d0c8f796063b
Victoriagjolstad <116063769+Victoriagjolstad@users.noreply.github.com>
2022-12-25 16:57:32 +0100

    Create forsøkpåloop.js

---
+++ Uke 5/Oppgave/forsøkpåloop.js
@@ -0,0 +1,9 @@
+let reverseString = "hello!";
+function reverseText () {
+for (let i = 1; i < reverseString.length; i--) {
+    text = reverseString.charAt(i);
+    reverseString += text;
+    System.out.println(resault);
+    console.log(reverseString);
+}}
+console.log(reverseText);
